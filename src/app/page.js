1cbd7e8843604c06d3e811b98d8ea5ab414797e6
Doni Anggun Yapto Nugroho <donianggunyaptonugroho@NB-DoniNugroho-Tech.local>
2024-08-26 13:57:10 +0700

    add promo page button

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import TodoList from '../components/TodoList';
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4 rounded-t-lg mt-2 bg-white">
+    <div className="container mx-auto p-4 bg-white">
       <Head>
         <title>To-Do List</title>
         <meta name="description" content="A simple to-do list app with Next.js and Tailwind CSS" />
